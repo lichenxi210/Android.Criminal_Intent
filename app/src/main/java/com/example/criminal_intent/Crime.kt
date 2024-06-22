11e5e4686ffa0862d83af61af3e0ace1cb31da2c
li'chen'xi <210647901@qq.com>
2024-06-22 15:48:01 +0800

    Initial:1

---
+++ app/src/main/java/com/example/criminal_intent/Crime.kt
@@ -0,0 +1,10 @@
+package com.example.criminal_intent
+
+import android.service.quicksettings.Tile
+import java.util.Date
+import java.util.UUID
+
+data class Crime (val id:UUID = UUID.randomUUID(),
+                  var title:String ="",
+                  var date:Date =Date(),
+                  var isSolved: Boolean = false)
