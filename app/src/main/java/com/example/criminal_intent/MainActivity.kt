c4138f9b99121e4c426ed42ef39b45c49c2892aa
thesubconscious <460356823@qq.com>
2024-06-24 00:06:09 +0800

    项目结构大改（工整化），实现了数据持化的相关功能

--- app/src/main/java/com/example/criminal_intent/MainActivity.kt
+++ app/src/main/java/com/example/criminal_intent/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.criminal_intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.criminal_intent.ui.fragment.CrimeListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
