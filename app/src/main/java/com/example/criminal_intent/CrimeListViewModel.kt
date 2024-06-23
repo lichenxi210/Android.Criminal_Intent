38c60f4abd34d55b06a00e06134692cab0056a57
li'chen'xi <210647901@qq.com>
2024-06-23 11:26:41 +0800

    李晨希的搜索功能已完成

--- app/src/main/java/com/example/criminal_intent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminal_intent/CrimeListViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.criminal_intent
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class CrimeListViewModel:ViewModel() {
-    val crimes = mutableListOf<Crime>()
+class CrimeListViewModel : ViewModel() {
+    private val _crimeListLiveData = MutableLiveData<List<Crime>>()
+    val crimeListLiveData: LiveData<List<Crime>>
+        get() = _crimeListLiveData
 
     init {
-        for (i in 0  until 100){
+        val crimes = mutableListOf<Crime>()
+        for (i in 0 until 100) {
             val crime = Crime()
-            crime.title ="Crime #$i"
-            crime.isSolved = i%2 == 0
+            crime.title = "Crime #$i"
+            crime.isSolved = i % 2 == 0
             crimes += crime
-
         }
+        _crimeListLiveData.value = crimes
     }
 }
